ab02e336cbd93e7df38ef71654c8dfdd80a0ed9e
zaheer310 <mohdabdulzaheer6969@gmail.com>
2026-07-11 14:26:01 +0530

    Shimmer Effect

---
+++ Day_2/src/utils/constant.js
@@ -0,0 +1,1 @@
+export const swiggyURL = "https://corsproxy.io/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D17.38430%26lng%3D78.45830%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
